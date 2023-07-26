1dc76cb6be3da102f474b69431e8b22f89443261
= <hardlane2000@gmail.com>
2023-07-26 07:11:34 +0200

    adding props to components

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -8,7 +8,7 @@ function Banner() {
       <div className='absolute top-1/2 w-full text-center'>
         <p className='text-sm sm:text-lg'>Not sure where to go? Perfect.</p>
         <button className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>
-          I'm flexible.
+          Check Our Hotels
         </button>
       </div>
     </div>
